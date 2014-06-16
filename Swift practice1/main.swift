1ad0b0a0c5429a511d1146e6a3bb74f93f5b6598
Wayne Huang <wayne@dh.clt.gocyberlink.com>
2014-06-16 20:00:12 +0800

    Initial commit from local git'
    '

---
+++ Swift practice1/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Swift practice1
+//
+//  Created by Wayne Huang on 6/15/14.
+//  Copyright (c) 2014 Wayne Huang. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+
+var name: String = "王小明"
+var age: Int = 3
+var height: Double = 50
+println("Name:\(name) Age:\(age)")
+
+
+let pi: Double = 3.1415967
+
+println("PI: \(pi)")
